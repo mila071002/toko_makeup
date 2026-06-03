caf1fd51c2c419eaae51ec7a65fa3b4a63de179c
Mila Johana <milajohana123@gmail.com>
2026-06-03 23:27:36 +0700

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //tampilkan products:
+        $products = \App\Models\product::all();
+        return view('products.index',compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //menyimpan data:
+        $request->validate(
+            [
+                'nama_makeup' => 'required',
+                'harga' => 'required|numeric',
+                'stok' => 'required|numeric',
+                'deskripsi' => 'required'
+            ]
+        );
+        \App\Models\Product::create($request->all());
+        return redirect()->route('products.index')->with('success','barang berhasil ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //validasi :
+        $request->validate(
+            [
+                'nama_makeup' => 'required',
+                'harga' => 'required|numeric',
+                'stok' => 'required|numeric',
+                'deskripsi' => 'required'
+            ]
+        );
+        //cari produk berdasarkan ID:
+        $product = \App\Models\Product::findOrFail($id);
+        //update:
+        $product->update($request->all());
+        return redirect()->route('products.index')->with('success','barang berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //hapus data berdasarkan id
+        $product = \App\Models\Product::findOrFail($id);
+        $product->delete();
+        return redirect()->route('products.index')->with('success','barang berhasil dihapus');
+    }
+}
