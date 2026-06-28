97e1a72e94af5f2ca7a131d786cf6281c39361af
Mila Johana <milajohana123@gmail.com>
2026-06-28 19:49:36 +0700

    Menambahkan fitur transaksi

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 
 //halaman utama langsung ke login
 Route::get('/',[AuthController::class,'showLogin'])->name('login');
@@ -10,8 +11,10 @@ Route::post('/login',[AuthController::class,'login'])->name('login.process');
 Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
 
-//protect halaman product supaya tidak bisa di akses tanpa login
+//protect halaman products supaya tidak bisa diakses tanpa login
 Route::middleware('auth')->group(function(){
     Route::get('/products/download-pdf',[ProductController::class,'downloadPdf'])->name('products.pdf');
     Route::resource('products',ProductController::class);
+    Route::get('transactions',[TransactionController::class,'index'])->name('transactions.index');
+    Route::post('transactions',[TransactionController::class,'store'])->name('transactions.store');
 });
